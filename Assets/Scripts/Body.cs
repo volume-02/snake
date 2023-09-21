f41069f9da202ba2b61bf34bafbfc6c7fe7ab7ae
volume02 <borsukboris@gmail.com>
2023-09-21 23:17:39 +0700

    fix tail movement

--- Assets/Scripts/Body.cs
+++ Assets/Scripts/Body.cs
@@ -5,52 +5,21 @@ using UnityEngine;
 public class Body : MonoBehaviour
 {
     public GameObject next;
-    private Vector3 direction;
+    public Vector3 direction;
+
     // Start is called before the first frame update
     void Start()
     {
-        CalculateDirection();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(direction * Time.deltaTime * 5);
+        var offsetValue = GetComponent<MeshRenderer>().bounds.size.x / 2;
+        var diff = next.transform.position - transform.position;
+        var trans = diff - diff.normalized * offsetValue;
+        transform.Translate(trans);
 
-        if (direction == Vector3.zero ||
-            direction == Vector3.forward && transform.position.z >= next.transform.position.z ||
-            direction == Vector3.back && transform.position.z <= next.transform.position.z ||
-            direction == Vector3.right && transform.position.x >= next.transform.position.x ||
-            direction == Vector3.left && transform.position.x <= next.transform.position.x
-
-            )
-        {
-            CalculateDirection();
-        }
-    }
-
-    private void CalculateDirection()
-    {
-        float horizontalInput = next.transform.position.x - transform.position.x ;
-        float verticalInput = next.transform.position.z - transform.position.z;
-
-        horizontalInput = Mathf.Abs(horizontalInput) < 0.1 ? 0 : horizontalInput;
-        verticalInput = Mathf.Abs(verticalInput) < 0.1 ? 0 : verticalInput;
-
-        if (verticalInput > 0)
-        {
-            direction = Vector3.forward;
-        } else if (verticalInput < 0)
-        {
-            direction = Vector3.back;
-        }
-        else if (horizontalInput > 0)
-        {
-            direction = Vector3.right;
-        }
-        else if (horizontalInput < 0)
-        {
-            direction = Vector3.left;
-        }   
+        direction = diff.normalized;
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,21 +10,17 @@ public class Player : MonoBehaviour
     private GameObject tail;
 
     private Vector3 tailDirection;
-    private Vector3 lastTailPos;
     // Start is called before the first frame update
     void Start()
     {
         tail = gameObject;
-        lastTailPos = tail.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         MoveHead();
-        var currTailPos = tail.transform.position;
-        CalculateTailDirection(currTailPos, lastTailPos);
-        lastTailPos = tail.transform.position;
+        tailDirection = tail?.GetComponent<Body>()?.direction ?? headDirection;
 
     }
 
@@ -32,6 +28,7 @@ public class Player : MonoBehaviour
     {
         float verticalInput = Input.GetAxis("Vertical");
         float horizontalInput = Input.GetAxis("Horizontal");
+
 
         if (verticalInput > 0 && headDirection != Vector3.back)
         {
@@ -54,50 +51,11 @@ public class Player : MonoBehaviour
         transform.Translate(headDirection * speed * Time.deltaTime);
     }
 
-    private void CalculateTailDirection(Vector3 currPos, Vector3 lastPos)
-    {
-        var zDirection = Mathf.Abs(lastPos.z) < currPos.z ? Vector3.forward : Vector3.back;
-        var xDirection = Mathf.Abs(lastPos.x) < currPos.x ? Vector3.right : Vector3.left;
-
-        if (lastPos.z == currPos.z)
-        {
-            tailDirection = xDirection;
-        }
-        else if (lastPos.x == currPos.x)
-        {
-            tailDirection = zDirection;
-        }
-    }
-
     private Vector3 CalculateOffset()
     {
         var offsetValue = GetComponent<MeshRenderer>().bounds.size.x;
-       
-        if (tailDirection == Vector3.forward)
-        {
-            Debug.Log("forward");
-            return tail.transform.position - new Vector3(0, 0, offsetValue);
 
-        }
-        else if (tailDirection == Vector3.back)
-        {
-
-            Debug.Log("back");
-            return tail.transform.position - new Vector3(0, 0, -offsetValue);
-        }
-        else if (tailDirection == Vector3.right)
-        {
-
-            Debug.Log("right");
-            return tail.transform.position - new Vector3(offsetValue, 0, 0);
-        }
-        else if (tailDirection == Vector3.left)
-        {
-
-            Debug.Log("left");
-            return tail.transform.position - new Vector3(-offsetValue, 0, 0);
-        } 
-        return Vector3.zero;
+        return tail.transform.position - offsetValue * tailDirection;
     }
 
     private void OnTriggerEnter(Collider other)
